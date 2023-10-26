be22792e928dab65b441f4ff5acdc87932ed1ee1
Zion Awino <awinozion85@gmail.com>
2023-10-26 12:36:40 +0200

    few changes added the public api and a button on the form

--- examination/application/app.py
+++ examination/application/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request
+import urllib.request
+import ssl
+import json
+import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,5 +16,20 @@ def form():
     """This is where my form html site is rendered
     """
     return render_template("form.html")
+
+@app.route("/api", methods=["POST"])
+def api_post():
+    ÅR = request.form["year"]
+    MÅNAD = request.form["month"]
+    DAG=request.form["day"]
+
+    context = ssl._create_unverified_context()
+    data_url = f"https://www.elprisetjustnu.se/elpris-api"
+    json_data = urllib.request.urlopen(data_url,context=context).read()
+    data = json.loads(json_data)
+    df = pd.DataFrame(data)
+    table_data = df.to_html(columns=["date","localName"],classes="table p-5",justify="left")
+    return render_template("index.html",data=table_data)
+
 
 
